fa9a40bcdcf60e1de2a6febeff20c2a3ba56ea4b
mohammed-iqyan-15052002 <iqyanmohammed@gmail.com>
2024-06-08 14:49:13 +0530

    All changes

--- script.js
+++ script.js
@@ -1,72 +1,161 @@
-let tl=gsap.timeline()
+let tl = gsap.timeline()
 
-tl.to('.cont',{
-    y:-500,
+tl.to('.cont', {
+    y: -500,
+    opacity: 0,
+    duration: 3,
+    delay: 1,
+    scrub: 2,
+    pin: true
+})
+
+tl.from('.p ', {
+    y: -200,
+    opacity: 0,
+
+})
+
+tl.from('.navbar li', {
+    y: -200,
+    opacity: 0,
+
+})
+
+tl.from('.img img', {
+    x: 100,
+    opacity: 0,
+})
+
+gsap.from('.text h1', {
+    x: -200,
+    opacity: 0,
+    duration: 2,
+    delay: 5
+})
+
+gsap.from('.text button', {
+    x: -200,
+    opacity: 0,
+    duration: 2,
+    delay: 5
+})
+
+gsap.to('.cont h1', {
+    y: -200,
+    opacity: 0,
+    duration: 3,
+    delay: 1,
+    stagger: 0.7
+
+})
+
+gsap.from('.imgs ', {
+    x: -200,
+    stagger: 1,
+    opacity: 0,
+    delay: 0.5,
+    duration: 1,
+    scrollTrigger: {
+        trigger: '.third span',
+        scroller: 'body',
+        start: 'top 10%'
+    },
+    pin: true
+})
+
+gsap.from('.second h1', {
+    transform: 'translate(-150%)',
+    scrollTrigger: {
+        trigger: '.second ',
+        scroller: 'body',
+        start: 'top 0%',
+        end: 'top -100% ',
+        scrub: 3,
+    },
+    delay: 2,
+    duration: 3
+})
+
+
+gsap.from('.percent p', {
+    x:-300,
+    stagger: 0,
     opacity:0,
-    duration:3,
     delay:1,
-    scrub:2,
-    pin:true
-})
-
-tl.from('.p p',{
-    y:-200,
-    opacity:0,
-
-})
-
-tl.from('.navbar li',{
-    y:-200,
-    opacity:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:'.myimg #p2',
+        scroller:'body',
+        
+    }
 
 })
 
-tl.from('img',{
-    x:100,
-    opacity:0,
+
+
+let images = document.querySelectorAll('.images');
+
+images.forEach(function (image) {
+    image.addEventListener('mouseover', function () {
+        let value = image.querySelector('.value');
+        if (value) {
+            value.style.marginTop = '50%';
+        }
+    });
+
+    image.addEventListener('mouseout', function () {
+        let value = image.querySelector('.value');
+        if (value) {
+            value.style.marginTop = '70%';
+        }
+    });
+});
+
+
+let skill = document.getElementById('mainskill');
+let exp = document.getElementById('Experiance');
+let ed = document.getElementById('Education');
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
+skill.style.display = 'block';
+exp.style.display = 'none';
+ed.style.display = 'none';
+
+btn1.addEventListener('click', () => {
+    setTimeout(() => {
+        skill.style.display = 'block';
+        exp.style.display = 'none';
+        ed.style.display = 'none';
+        btn1.style.background = ' var(--lightest-navy)';
+        btn2.style.background = 'transparent';
+        btn3.style.background = ' transparent';
+    }, 500)
+
 })
 
+btn2.addEventListener('click', () => {
+    setTimeout(() => {
+        exp.style.display = 'block'
+        skill.style.display = 'none'
+        ed.style.display = 'none';
+        btn2.style.background = ' var(--lightest-navy)';
+        btn1.style.background = 'transparent';
+        btn3.style.background = 'transparent';
+    }, 500)
 
-gsap.from('.text h1',{
-    x:-200,
-    opacity:0,
-    duration:2,
-    delay:5
 })
 
-gsap.from('.text button',{
-    x:-200,
-    opacity:0,
-    duration:2,
-    delay:5
-})
+btn3.addEventListener('click', () => {
+    setTimeout(() => {
+        exp.style.display = 'none'
+        skill.style.display = 'none'
+        ed.style.display = 'block'
+        btn1.style.background = 'transparent';
+        btn2.style.background = ' transparent';
+        btn3.style.background = 'var(--lightest-navy)'
+    }, 500)
 
-
-
-gsap.to('.cont h1',{
-    y:-200,
-    opacity:0,
-    duration:3,
-    delay:1,
-    stagger:0.7
-  
-})
-
-
-let container=document.querySelector('body')
-let mouse=document.getElementById('mouse')
-mouse.style.display="none"
-
-container.addEventListener('mousemove',function(val){
-  setTimeout(()=>{
-    mouse.style.display='block'
-      gsap.to(mouse,{
-        x:val.x,
-        y:val.y,
-        // duration:1
-        ease:'back.out'
-       })
-  },100) 
 })
 
 
